9e9c8032f74d06d1cd27369bfe7fb31091913263
pguenzi-tiberi <pierre.guenzi.tiberi@gmail.com>
2023-04-21 15:16:25 +0200

    options written

---
+++ Alien_index_report_generator.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import sys
+import time
+import shutil
+import pandas as pd
+from sympy import im 
+from lib import merging
+from lib import prepare
+from lib import separiting_file
+
+
+def run() :
+    parser = argparse.ArgumentParser(
+        prog="AiRG",
+        description="\n\n Alien index report Generator to detect HGT ",
+        formatter_class=argparse.RawTextHelpFormatter,
+        add_help=True,
+    )
+
+    mandatory_args = parser.add_argument_group("Mandatory arguments")
+
+    # Mandatory arguments
+
+    # Trinotate report
+    
+    mandatory_args.add_argument(
+        "--report",
+        action="store",
+        dest="blast_report ",
+        help="blast (-outfmt 6) or diamond (--outfmt 6) report",
+        default="",
+        required=True,
+    )
+
+    mandatory_args.add_argument(
+        "--id",
+        action="store",
+        dest="id_prot",
+        help="a file (as in test directory) with only id protein (in one copy)",
+        default="",
+        required=True,
+    )
+
+    mandatory_args.add_argument(
+        "--kingdom",
+        action="store",
+        dest="kingdom",
+        help="The kingdom of your organism. It has to be written as in your blast / diamond report",
+        default="",
+        required=True,
+    )
+    
+    # Output
+    optional_args.add_argument(
+        "--output",
+        "-o",
+        action="store",
+        dest="output_dir",
+        help="Output directory name",
+        default="results",
+        required=False,
+    )
+
+    args = parser.parse_args()
+
+    try:
+        os.mkdir(args.output_dir)
+    except:
+        print(
+            f"\n Output directory {args.output_dir} can not be created, please erase it before launching the programm !"
+        )
+        sys.exit(1)
+
+    actual_path = os.getcwd()
+    os.chdir(args.output_dir)
+
+    global_start = time.perf_counter()
+
+
+    print(
+        f"\n Total running time : {float(time.perf_counter() - global_start)} seconds"
+    )
+            
+if __name__ == '__main__':
+    run()
